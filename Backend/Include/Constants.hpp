05992630502d27c35a94f9503660eab9a24b1650
Aditya Kumawat <adityakumawat2003@gmail.com>
2026-03-03 23:32:37 +0530

    structure created

---
+++ Backend/Include/Constants.hpp
@@ -0,0 +1,9 @@
+#include <string>
+using namespace std;
+namespace FlowShield {
+    const int HEARTBEAT_INTERVAL = 5;      // Seconds between each "Image Drop"
+    const string CLIENT_INBOX = "../storage_system/node_client/";
+    const string RELAY_1_PATH = "../storage_system/node_relay_1/";
+    const string RELAY_2_PATH = "../storage_system/node_relay_2/";
+    const string COVER_IMAGE_POOL = "./assets/covers/"; // Folder full of Cats/Dogs
+}
